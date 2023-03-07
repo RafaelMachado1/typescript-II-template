c3e6b070579a6d70833d380ad21eea23ee3b48b0
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2023-03-06 22:07:04 -0300

    pratica-guiada-concluido

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,31 @@ Crie um sistema de cadastro de usuários que contenha:
 */
 
 
+/* type Person = {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  role: ROLE
+} */
 
+/* const userOne:Person = {
+  id: "0101",
+  name: "Juliana",
+  email: "juliana@email.com",
+  password: "25485",
+  role: ROLE.ADMIN
+} */
 
+type AdminAccount = {
+  account: string,
+  permission: true
+}
 
-
-
-
-
-
-
-
-
-
+type NormalAccount = {
+  account: string,
+  permission: false
+}
 
 
 /* PRÁTICA GUIADA - Parte 2
@@ -31,4 +44,88 @@ Vamos continuar nosso sistema de cadastro de usuários criando:
 4. Crie duas pessoas, uma com permissão normal e a outra admin;
 5. Guarde essas pessoas no array de usuários.
 
-*/ 
+*/
+
+//2.1 ENUM
+
+type Person = {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  role: ROLE
+}
+
+enum ROLE {
+  ADMIN = "Admin",
+  NORMAL = "Normal"
+}
+
+const userThree: Person = {
+  id: "0303",
+  name: "Jaziel",
+  email: "Jaziel@email.com",
+  password: "25485",
+  role: ROLE.NORMAL
+}
+
+const userFour: Person = {
+  id: "0404",
+  name: "Camila",
+  email: "Camila@email.com",
+  password: "25485",
+  role: ROLE.ADMIN
+}
+
+//2. Tipo Intersection unindo: pessoa(Person) + permissão (Role);
+
+type AdminUser = Person & AdminAccount
+type NormalUser = Person & NormalAccount
+
+/* type AdminUser {
+  id: string,
+  name: string,
+  email: string,
+  password: string,
+  role: ROLE,
+  account: string,
+  permission: true
+} */
+
+
+
+//3. Um array de usuários que permite guardar apenas usuários do tipo Person + Role;
+
+const arrayAdmimUser: AdminUser[] = []
+const arrayNormalUser: NormalUser[] = []
+
+//4. Crie duas pessoas, uma com permissão normal e a outra admin;
+
+const userFive: AdminUser = {
+  id: "0505",
+  name: "Monica",
+  email: "monica@email.com",
+  password: "123456",
+  role: ROLE.ADMIN,
+  account: "administrador",
+  permission: true
+}
+
+const userSix: NormalUser = {
+  id: "0606",
+  name: "Cebolinha",
+  email: "cebolinha@email.com",
+  password: "123456",
+  role: ROLE.NORMAL,
+  account: "normal",
+  permission: false
+}
+
+//const arrayAdmimUser: AdminUser[] = [userFive, userSix]
+
+const arrayUser: (AdminUser | NormalUser)[] = [userFive, userSix]
+
+//arrayUser.push(userFive)
+
+console.table(arrayUser)
+
